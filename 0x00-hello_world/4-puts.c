d1eca5d78827c5cb075e4829383ce0672748dd0a
Engy Saleh <engysaleh12@gmail.com>
2023-09-07 04:42:35 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,13 @@
 /**
  * main - entry point
  *
- * Description : Write a C program use the function puts
+ * Description: Write a C program use the function puts
  *
- * Return : Always (0)
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
