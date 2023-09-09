fda1ab64e022377150dfd2f716373e4ac5c1aea2
Engy Saleh <engysaleh12@gmail.com>
2023-09-09 00:23:15 -0700

    print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,9 +14,9 @@ int main(void)
 
 	for (x = 0 ; x <= 9 ; x++)
 	{
-		for (y = 0 ; y <= 9 ; y++)
+		for (y = x + 1 ; y <= 9 ; y++)
 		{
-			for (z = 0 ; z <= 9 ; z++)
+			for (z = y + 1 ; z <= 9 ; z++)
 			{
 				putchar(x + '0');
 				putchar(y + '0');
