dfeab0376dd6fe4ce6e33b1a6235dd0f0a751b64
Engy Saleh <engysaleh12@gmail.com>
2023-09-12 17:16:59 -0700

    print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * Description: prints 10 times the alphabet in lowercase
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char x;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+			_putchar(x);
+		_putchar('\n');
+	}
+	return (0);
+}
