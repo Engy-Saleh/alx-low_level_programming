2a99a40b4b2e4949b190170decc53393adc08c64
Engy Saleh <engysaleh12@gmail.com>
2023-09-17 11:13:51 -0700

    print_line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,12 +12,15 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0 ; i < n ; i++)
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
-		if (n > 0)
+		for (i = 1; i <= n; i++)
+		{
 			_putchar('_');
-		else
-			_putchar('\n');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
+
 }
