97542316365db82cabdd810c03d04827a5769606
engysaleh <engysaleh12@gmail.com>
2023-10-21 23:35:17 -0700

    8-24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,16 +12,16 @@ void jack_bauer(void)
 {
 	int hr, min;
 
-	for (hr = 0; hr <= 23; hr++)
+	for (hr = 0; hr < 24; hr++)
 	{
-		for (min = 0; min <= 59; min++)
+		for (min = 0; min < 60; min++)
 		{
-			_putchar((hr / 10) + '0');
-			_putchar((hr % 10) + '0');
-			_putchar(':');
-			_putchar((min / 10) + '0');
-			_putchar((min % 10) + '0');
-			_putchar('\n');
+			_putchar ((hr / 10) + '0');
+			_putchar ((hr % 10) + '0');
+			_putchar (':');
+			_putchar ((min / 10) + '0');
+			_putchar ((min % 10) + '0');
+			_putchar ('\n');
 		}
 	}
 }
