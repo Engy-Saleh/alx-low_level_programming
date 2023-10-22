98e95d1f1d362323b8189b8084e0ec2c699a6881
engysaleh <engysaleh12@gmail.com>
2023-10-22 00:38:31 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++ )
+		for (j = 0; j < 10; j++)
 		{
 			k = i * j;
 			if (j == 0)
@@ -34,7 +34,7 @@ void times_table(void)
 				_putchar (' ');
 				_putchar ((k / 10) + '0');
 				_putchar ((k % 10) + '0');
-			}	
+			}
 		}
 		_putchar ('\n');
 	}
