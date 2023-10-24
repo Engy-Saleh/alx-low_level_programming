c37f73364d74bf2a22114e76d62c3da904b33284
engysaleh <engysaleh12@gmail.com>
2023-10-24 05:12:05 -0700

    7-puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,11 +10,12 @@
 
 void puts_half(char *str)
 {
-	int i, n;
+	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 		;
-	i++
+
+	i++;
 	for (i /= 0; str[i]!= '\0'; i++)
 		_putchar (str[i]);
 	_putchar ('\n');
