8791dd8f654fa430b1e4ba46328f27eede5184e9
engysaleh <engysaleh12@gmail.com>
2023-10-24 02:01:34 -0700

    6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,3 @@
-#include "stdio.h"
 #include "main.h"
 
 /**
@@ -14,7 +13,9 @@ void puts2(char *str)
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
-
-	if (i % 2 == 0)
-		printf("%c\n", str[i]);
+	{
+		if (i % 2 == 0)
+			_putchar (str[i]);
+	}
+	_putchar ('\n');
 }
