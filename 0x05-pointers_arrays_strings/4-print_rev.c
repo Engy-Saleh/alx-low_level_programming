cc91a36d4eca6c8f149a5a02677b7b638233e547
engysaleh <engysaleh12@gmail.com>
2024-01-05 23:35:27 -0800

    print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,13 +12,12 @@ void print_rev(char *s)
 {
 	int i;
 
+	i = 0;
 	while (*s != '\0')
-		s++;
+		i++;
 
-	for (i -= 1; i > 0; i--)
-	{
+	for (i -= 1; i >= 0; i--)
 		_putchar(*s);
-		i--;
-	}
+
 	_putchar('\n');
 }
