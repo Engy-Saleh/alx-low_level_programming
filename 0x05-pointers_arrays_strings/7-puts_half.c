fdb4be9e6e0365ff16874d8a2cdae49373801e42
engysaleh <engysaleh12@gmail.com>
2024-01-07 01:02:36 -0800

    puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,15 +8,12 @@
  * Return: Nothing
  */
 
-void puts_half(char *str)
+void puts_half(char *s)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-		;
-
-	i++;
-	for (i /= 2; str[i] != '\0'; i++)
-		_putchar (str[i]);
-	_putchar ('\n');
+	i = 0;
+	for (i /= 0; s[i] != '\0'; i++)
+		_putchar(s[i]);
+	_putchar('\n');
 }
