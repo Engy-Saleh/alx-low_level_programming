87624c263b71f5e3214ec74ddc25fb640fe8c0df
engysaleh <engysaleh12@gmail.com>
2024-01-08 05:52:53 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,21 +8,19 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int x;
-	int y;
+	int x, y;
 
-	/*find dest size*/
-	x = 0;
-	while (dest[x] != '\0')
-		x++;
+	for (x = 0; dest != '\0'; x++)
+		;
 
-	y = 0;
-	while (src[y] != '\0')
-	{
-		dest[x] = src[y];
-		x++;
-		y++;
-	}
+	for (Y = 0; src != '\0'; y++)
+		;
+
+	dest[x] = src[y];
+	x++;
+	y++;
+
 	dest[x] = '\0';
+
 	return (dest);
 }
