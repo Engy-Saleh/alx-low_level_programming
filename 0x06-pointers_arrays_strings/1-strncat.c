76d5332166e34448b4898b1eb04f7923f6d4503d
engysaleh <engysaleh12@gmail.com>
2024-01-08 22:45:22 -0800

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,10 +17,9 @@ char *_strncat(char *dest, char *src, int n)
 		;
 
 	for (y = 0; y < n && src[y] != '\0'; y++)
-	{
+
 		dest[x + y] = src[y];
-		x++;
-	}
+
 	dest[x + y] = '\0';
 
 	return (dest);
