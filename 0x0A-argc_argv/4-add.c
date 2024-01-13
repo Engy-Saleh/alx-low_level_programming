c2e454d237f7c03d98519c1885ea53155569cdf7
Engy-Saleh <engysaleh12@gmail.com>
2024-01-12 23:33:02 -0800

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,26 +9,32 @@
  *
  * Return: 0 (Success), or 1 (Success)
  */
+
 int main(int argc, char *argv[])
 {
-	int *c, sum = 0;
+    int *c, sum = 0;
 
-	if  (argc == 1)
-		printf("o\n");
+    if (argc == 1)
+    {
+        printf("0\n");
+        return 0;
+    }
 
-	while (argc > 1)
-	{
-		for (c = argv[argc], *c != '\0', c++)
-		{
-			if (*c < '0' || *c > '9')
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		sum += atoi(argv[argc]);
-		arg--;
-		printf("%d\n",sum);
-	}
-	return (0);
+    while (argc > 1)
+    {
+        for (c = argv[argc]; *c != '\0'; c++)
+        {
+            if (*c < '0' || *c > '9')
+            {
+                printf("Error\n");
+                return 1;
+            }
+        }
+
+        sum += atoi(argv[argc]);
+        argc--;
+        printf("%d\n", sum);
+    }
+
+    return 0;
 }
