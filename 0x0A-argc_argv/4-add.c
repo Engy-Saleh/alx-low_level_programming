f3c89e8991e9cf736fe23e2b0c8d4284d0abc57e
Engy-Saleh <engysaleh12@gmail.com>
2024-01-12 22:26:29 -0800

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,51 +2,6 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
- */
-int _atoi(char *s)
-{
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
-}
-
-/**
  * main - adds two positive number
  * @argc: number of arguments
  * @argv: array of arguents
@@ -55,31 +10,27 @@ int _atoi(char *s)
  */
 int main(int argc, char *argv[])
 {
-	int sum, num, i, j, k;
+	int *c, sum = 0;
 
-	sum = 0;
+	if  (argc == 0)
+		printf("o\n");
 
-	for (i = 1; i < argc; i++)
+	while (argc -1)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		for (c = argv[argc], *c, c++)
 		{
-			if (argv[i][j] > '9' || argv[i][j] < '0')
+			if (*c < '0' || *c > '9')
 			{
-				puts("Error");
+				printf("Error\n");
 				return (1);
+			}
+
+			else
+			{
+				sum += atoi(argv[argc]);
+				printf("%d\n",sum)
 			}
 		}
 	}
-
-	for (k = 1; k < argc; k++)
-	{
-		num = _atoi(argv[k]);
-		if (num >= 0)
-		{
-			sum += num;
-		}
-	}
-
-	printf("%d\n", sum);
 	return (0);
 }
