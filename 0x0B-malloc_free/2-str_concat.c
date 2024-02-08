b9675845ea914b568b1d02a8e8bd29529f9c2aa2
Engy-Saleh <engysaleh12@gmail.com>
2024-02-08 05:19:54 -0800

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,32 +11,31 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *concat;
-	int i = 0, k = 0;
-
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[i] != '\0')
-		i++;
+	int len_s1 = 0, len_s2 = 0;
+	
+	while (s1[len_s1] != '\0')
+		len_s1++;
+	while (s2[len_s2] != '\0')
+		len_s2++;
 
-	while (s2[k] != '\0')
-		 k++;
+	char *result = (char *)malloc((len_s1 + len_s2 + 1) * sizeof(char));
 
-	concat = malloc(sizeof(char) * (i + k + 1));
+	if (result == NULL) // Memory allocation failed
+		return NULL;
 
-	if (concat == NULL)
-		return (NULL);
+	for (int i = 0; i < len_s1; i++)
+		result[i] = s1[i];
 
-	for (int j = 0; j < i; j++)
-		concat[j] = s1[j];
+	for (int i = 0; i < len_s2; i++)
+		result[len_s1 + i] = s2[i];
 
-	for (int j = 0; j < k; j++)
-		concat[i + j] = s2[j];
+	result[len_s1 + len_s2] = '\0';
 
-	concat[i + k] = '\0';
-	return (concat);
+	return result;
 }
