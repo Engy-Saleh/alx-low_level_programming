1cad34f7d301bd2c8d34e9b76ff836a2bcb6d7c7
Engy-Saleh <engysaleh12@gmail.com>
2024-02-08 05:22:07 -0800

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,31 +11,47 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - concatenates two strings.
+ * @s1: input one to concat
+ * @s2: input two to concat
+ *
+ * Return: concat of s1 and s2
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	int i, ci;
+
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
 
-	int len_s1 = 0, len_s2 = 0;
-	
-	while (s1[len_s1] != '\0')
-		len_s1++;
-	while (s2[len_s2] != '\0')
-		len_s2++;
+	i = ci = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[ci] != '\0')
+		ci++;
+	concat = malloc(sizeof(char) * (i + ci + 1));
 
-	char *result = (char *)malloc((len_s1 + len_s2 + 1) * sizeof(char));
+	if (concat == NULL)
+		return (NULL);
+	i = ci = 0;
+	while (s1[i] != '\0')
+	{
+		concat[i] = s1[i];
+		i++;
+	}
 
-	if (result == NULL) // Memory allocation failed
-		return NULL;
-
-	for (int i = 0; i < len_s1; i++)
-		result[i] = s1[i];
-
-	for (int i = 0; i < len_s2; i++)
-		result[len_s1 + i] = s2[i];
-
-	result[len_s1 + len_s2] = '\0';
-
-	return result;
+	while (s2[ci] != '\0')
+	{
+		concat[i] = s2[ci];
+		i++, ci++;
+	}
+	concat[i] = '\0';
+	return (concat);
+}
 }
