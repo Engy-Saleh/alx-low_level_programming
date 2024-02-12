a2e16326c921e40f22b1a7a46b8acd90fe1c858f
Engy-Saleh <engysaleh12@gmail.com>
2024-02-12 03:31:14 -0800

    alloc_grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,28 +11,31 @@
  */
 int **alloc_grid(int width, int height)
 {
+
 	int **tab;
 	int i, j;
 
-	tab = malloc(sizeof(int*) * height);
-
 	if (width <= 0 || height <= 0)
-		return (NULL);
-	else
-	{
-		for (i = 0; i < height; i++)
-		{	tab[i] = malloc(sizeof(int) * width);
-				if (tab[i] == NULL)
-				{
-					for (j = 0; j < i; j++)
-						free(tab[i]);
-					free(tab);
-					return (NULL);
+		return NULL;
 
-				for (j = 0; j < width; j++)
-					tab[i][j];
-				}
-		}
+	tab = malloc(sizeof(int *) * height);
+	
+	if (tab == NULL)
+		return NULL;
+
+	for (i = 0; i < height; i++)
+	{
+		tab[i] = malloc(sizeof(int) * width);
+			if (tab[i] == NULL)
+			{
+					for (j = 0; j < i; j++)
+						free(tab[j]);
+					free(tab);
+					return NULL;
+			}
+
+			for (j = 0; j < width; j++)
+				tab[i][j] = 0;
 	}
-	return (tab);
+	return tab;
 }
