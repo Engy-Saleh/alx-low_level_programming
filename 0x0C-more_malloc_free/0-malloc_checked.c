ca901924cae3babe919667423d68cba56054ed99
Engy-Saleh <engysaleh12@gmail.com>
2024-02-13 00:30:47 -0800

    malloc_checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,12 +9,12 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	int *p;
 
-	ptr = malloc(b);
+	p = malloc(sizeof(int) * b);
 
-	if (ptr == NULL)
+	if (p == NULL)
 		exit(98);
 
-	return (ptr);
+	return(P);
 }
